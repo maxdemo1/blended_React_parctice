90cfc1b70690cf60e5d39a0f3eb5cd5651ab1205
boben195 <boben195@ukr.net>
2024-02-24 16:55:41 +0200

    solution task3

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,11 @@ import {
   CryptoHistory,
   ForbesList,
 } from 'components';
+
 import article from './data/article.json';
 import statistics from './data/stats.json';
+import forbes from 'data/forbes.json';
+
 export const App = () => {
   return (
     <Section>
@@ -28,7 +31,7 @@ export const App = () => {
         <Statistics title="Main Statistics" stats={statistics} />
 
         <Heading title="Task 3 Forbes list" top bottom />
-        <ForbesList />
+        <ForbesList list={forbes} />
 
         <Heading title="Task 4 Crypto history" top bottom />
         <CryptoHistory />
